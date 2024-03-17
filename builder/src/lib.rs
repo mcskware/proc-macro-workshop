f11ff64b09d6d2eb8137677304f0b7e926d7748f
mcskware <13559364+mcskware@users.noreply.github.com>
2024-03-17 16:57:22 +0100

    02 passing

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,13 +1,39 @@
-use proc_macro::TokenStream;
-use syn::{parse_macro_input, DeriveInput};
+use proc_macro::{Span, TokenStream};
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput, Ident};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let _ = input;
 
-    eprintln!("Derive input is: {input:#?}");
+    //eprintln!("Derive input is: {input:#?}");
 
     let derive_input = parse_macro_input!(input as DeriveInput);
 
-    TokenStream::new()
+    let struct_name = derive_input.ident;
+    let builder = Ident::new(&format!("{struct_name}Builder"), Span::call_site().into());
+
+    let quoted = quote!(
+        pub struct #builder {
+            executable: String,
+            args: Vec<String>,
+            env: Vec<String>,
+            current_dir: String,
+        }
+
+        impl #struct_name {
+            pub fn builder() -> #struct_name {
+                #struct_name {
+                    executable: String::new(),
+                    args: vec!(),
+                    env: vec!(),
+                    current_dir: String::new(),
+                }
+            }
+        }
+    );
+
+    //eprintln!("quoted is {quoted:#?}");
+
+    TokenStream::from(quoted)
 }
--- builder/tests/progress.rs
+++ builder/tests/progress.rs
@@ -1,11 +1,13 @@
 use derive_builder as _;
+use quote as _;
+use syn as _;
 
 #[test]
 fn tests() {
     let t = trybuild::TestCases::new();
     t.pass("tests/01-parse.rs");
-    //t.pass("tests/02-create-builder.rs");
-    //t.pass("tests/03-call-setters.rs");
+    t.pass("tests/02-create-builder.rs");
+    t.pass("tests/03-call-setters.rs");
     //t.pass("tests/04-call-build.rs");
     //t.pass("tests/05-method-chaining.rs");
     //t.pass("tests/06-optional-field.rs");
