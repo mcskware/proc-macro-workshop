f45b0caa2e73641c524804c00f6f45ff2a66d8bd
mcskware <13559364+mcskware@users.noreply.github.com>
2024-03-17 13:52:56 +0100

    01-parse tests passing

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,13 @@
 use proc_macro::TokenStream;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let _ = input;
 
-    unimplemented!()
+    eprintln!("Derive input is: {input:#?}");
+
+    let derive_input = parse_macro_input!(input as DeriveInput);
+
+    TokenStream::new()
 }
